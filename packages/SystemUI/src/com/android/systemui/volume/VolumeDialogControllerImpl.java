bc00ac7af154355381751b5781054b219f8562f0
Adnan Begovic <adnan@cyngn.com>
2013-11-04 00:58:57 +0000

    frameworks: Add unlinked ringtone and notification volumes
    Port over linked volumes from 12.1
    
    Implement linked volumes and add upgrade path.
    
    Change-Id: Ibbeff656fcaaa4a2ff79de530b48b216df271b21
    
    frameworks: read VOLUME_LINK_NOTIFICATION from proper table
    
    Change-Id: I15e9caa18f307a51a732c7a59013bbbb234a7477
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 1b7cfa1716017e0d03c9693428c21d8e11c5dc0a)
    
    fix Notification volume slider linking
    
    Change-Id: I0f74485f5c8c2aa7698cf9ba1c60c2c8c967798d
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Improve SeekBarVolumizer behavior with unlinked streams
    
    - Fix notification stream enabling at 0 volume
    - Fix the notification slider being set when moving ringer stream
    
    Change-Id: If3705cc833c07f726f8ad7b1ec79a42e86e8ff22
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    base: Fix notification sounds for wifi only devices
    
    The linking of voice & notification doesn't take into account the
    fact that a device may not actually have voice capabilities.  Whenever
    checking whether or not to link notification and ring volumes, also
    verify that the device really is voice capable.
    
    Change-Id: Iae49302d3b5934d36b182e289e4975f5dd85b834
    
    frameworks: fix setting up linked notification stream on boot
    
    updateStreamVolumeAlias() was previously responsible for checking the
    linked notification variable and mapping the notification stream, but
    the setting to read whether the notifications were linked was read in
    after that method had run which was responsible for linking them.
    Extract the logic and re-run it again after reading the persisted
    settings.
    
    Change-Id: Ia62bf3ca072def54ec6ebf4f9e865d9c913e3bc4
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    fixup volume stream linking
    
    - Make the SeekBarVolumizer move notification and ringer streams when
     linked and moving ringer.
    
    - Fix initial state causing volume stream seek bar to be set to 0
      initially
    
    - Clean up AudioService initialization code
    
    Change-Id: Ia11a7a392e5eb3ef862147843e365d5f20eaf90a
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: Fix volume panel inconsistencies
    
    * Use xml drawables everywhere
    * Add muted notification icon
    * Use AOSP naming scheme for drawables
    
    Change-Id: I14a3af9a7aecf4828f81a8e39855608e452848d6
    
    Misc fixes
    
    Change-Id: Ia94b0a6655e52f5949f031360f525c871cd8df14
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Use STREAM_RING on non-voice capable devices
    
    * That's what Google does.
    
    Change-Id: Iac88d161eb1dccbbb5c538d5e04d8d1fc43edef2
    (cherry picked from commit 981e89eb6b99c058a9b3bf533e6494ab44a4b241)
    
    VolumeDialog: Fix mismerge
    
    Fixes not disappearing cast pickers
    
    * Commit 568585b introduced a new LinearLayout for the volume rows
    * Remove unused booleans
    
    Change-Id: I0660d10872cc1098fdd67b618a20ea0f8e926e68
    
    Fix VOLUME_LINK_NOTIFICATION javadoc
    
    * Fixes checkapi:
      frameworks/base/core/java/android/provider/Settings.java:8920: error:
      Added field android.provider.Settings.Secure.VOLUME_LINK_NOTIFICATION [AddedField]
    
    Change-Id: I4da34de53ce23e90d5f2eff72ef0a15034f08ec1
    
    Change-Id: I7695aa15bc85e8ff19747ba7cd6700e190c7d988
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -525,6 +525,16 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
         return stream == AudioManager.STREAM_RING || stream == AudioManager.STREAM_NOTIFICATION;
     }
 
+    private boolean updateLinkNotificationConfigW() {
+        boolean linkNotificationWithVolume = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1;
+        if (mState.linkedNotification == linkNotificationWithVolume) {
+            return false;
+        }
+        mState.linkedNotification = linkNotificationWithVolume;
+        return true;
+    }
+
     private boolean updateEffectsSuppressorW(ComponentName effectsSuppressor) {
         if (Objects.equals(mState.effectsSuppressor, effectsSuppressor)) return false;
         mState.effectsSuppressor = effectsSuppressor;
@@ -893,6 +903,8 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
                 Settings.Global.getUriFor(Settings.Global.ZEN_MODE);
         private final Uri ZEN_MODE_CONFIG_URI =
                 Settings.Global.getUriFor(Settings.Global.ZEN_MODE_CONFIG_ETAG);
+        private final Uri VOLUME_LINK_NOTIFICATION_URI =
+                Settings.System.getUriFor(Settings.System.VOLUME_LINK_NOTIFICATION);
 
         public SettingObserver(Handler handler) {
             super(handler);
@@ -901,6 +913,8 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
         public void init() {
             mContext.getContentResolver().registerContentObserver(ZEN_MODE_URI, false, this);
             mContext.getContentResolver().registerContentObserver(ZEN_MODE_CONFIG_URI, false, this);
+            mContext.getContentResolver().registerContentObserver(VOLUME_LINK_NOTIFICATION_URI,
+                    false, this);
         }
 
         public void destroy() {
@@ -915,6 +929,9 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
             }
             if (ZEN_MODE_CONFIG_URI.equals(uri)) {
                 changed |= updateZenConfig();
+            }
+            if (VOLUME_LINK_NOTIFICATION_URI.equals(uri)) {
+                changed = updateLinkNotificationConfigW();
             }
 
             if (changed) {
