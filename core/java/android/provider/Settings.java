ce6c047a8fccafb7604ef57cf68479ab7839b088
00day0 <therandomuser11@gmail.com>
2020-01-01 22:54:26 +0100

    base: Allow to disable settings dashboard suggestions [1/3]
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4694,6 +4694,12 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Ability to show/hide settings dashboard suggestion cards
+         * @hide
+         */
+        public static final String SETTINGS_SUGGESTION_CARDS = "settings_suggestion_cards";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
